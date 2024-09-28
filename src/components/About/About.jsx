600d64e322a8fcbaf8b686ea95059031f3f92df5
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-09-28 17:37:15 +0530

    update about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,19 +12,14 @@ const About = () => {
         </div>
         <div className="about-info">
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-            voluptatum nobis qui ipsum praesentium nostrum dignissimos nam
-            debitis! Rem eum ipsum consequuntur autem temporibus natus similique
-            debitis perspiciatis excepturi voluptatibus ab, ducimus dolores,
-            vero, nemo soluta quos accusantium magni! Vel obcaecati eaque
-            voluptates, possimus provident quae labore distinctio officiis
-            beatae vero cum commodi. Nihil quo eaque quam veniam, sunt unde
-            ducimus temporibus nemo eligendi quae reprehenderit cum, eveniet
-            iusto repudiandae perspiciatis tenetur doloribus. Quo blanditiis
-            ipsam dolorum, in molestias quibusdam, libero reprehenderit eum
-            inventore autem distinctio reiciendis quas earum, nesciunt animi
-            quisquam sequi dolore eius. Pariatur similique recusandae molestias
-            excepturi.
+            Hello! I am currently an undergraduate student University of Colombo
+            School of Computing. I am passionate about software development, and I am constantly working on improving my
+            skills. I am a self-starter and take a proactive
+            approach to learning, often teaching myself new programming
+            languages and technologies in my free time. I am committed to always
+            giving my best effort. I am excited to continue learning and growing
+            as a programmer, and I am looking forward to seeing where my career
+            in the field will take me.
           </p>
         </div>
       </div>

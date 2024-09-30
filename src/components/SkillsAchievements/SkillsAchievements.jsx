003fc465165b127abd0e4287071daee49227f57e
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-09-30 21:04:01 +0530

    restructure and change the layout and css

--- src/components/SkillsAchievements/SkillsAchievements.jsx
+++ src/components/SkillsAchievements/SkillsAchievements.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import { skills, achievements } from "../../data";
 import "./SkillsAchievements.css";
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
 
 const SkillsAchievements = () => {
   return (
-    <section className="sections">
+    <section className="sections" id="achievements">
       <h1 className="title">My Achievements & Skills</h1>
       <div className="title-underline"></div>
 
@@ -12,15 +14,21 @@ const SkillsAchievements = () => {
           <h2 className="sub-title">Achievements</h2>
           <div className="achievements-grid">
             {achievements.map((achievement) => {
-              const { id, title, description, icon } = achievement;
+              const { id, title, company, description, icon } = achievement;
               return (
                 <div key={id} className="achievement-item">
                   <div className="achievement-icon">{icon}</div>
                   <p className="achievement-title">{title}</p>
+                  <p className="achievement-company">{company}</p>
                   <p className="achievement-description">{description}</p>
                 </div>
               );
             })}
+          </div>
+          <div className="achieve-btn-container">
+            <Link to="/achievements" className="achieve-btn">
+              <em>see more</em> <FaRegArrowAltCircleRight />
+            </Link>
           </div>
         </div>
 

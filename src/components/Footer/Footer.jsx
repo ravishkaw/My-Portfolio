a643122717e17e19361e0b70e73da49b97f7b61b
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-10-01 09:35:36 +0530

    rearrange footer and removed contact info

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,60 +6,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-container">
-        <img src={logo} alt="logo" className="nav-logo" />
-        <div className="footer-contact">
-          <h3>
-            Contact{" "}
-            <span className="new-window-icon">
-              <FaArrowUpRightFromSquare />
-            </span>
-          </h3>
-          <p>
-            <a href="mailto:ravishkawj@gmail.com">ravishkawj@gmail.com</a>
-          </p>
-          <p>
-            <a href="tel:+94775048662">+94 77 504 8662</a> {/* Optional */}
-          </p>
-        </div>
-
-        <div className="footer-social">
-          <h3>Follow Me</h3>
-          <ul className="social-icons">
-            <li>
-              <a
-                href="https://twitter.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-twitter"></i> Twitter
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://linkedin.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-linkedin"></i> LinkedIn
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-github"></i> GitHub
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div className="footer-bottom">
-        <p> &copy; {new Date().getFullYear()} Ravishka Wijerathne</p>
-      </div>
+      <p> &copy; {new Date().getFullYear()} Ravishka Wijerathne</p>
     </footer>
   );
 };

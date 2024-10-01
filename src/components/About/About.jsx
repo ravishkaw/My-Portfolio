54aff9b3b61aa8649baf3de88735a334a210a15e
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-10-01 09:34:23 +0530

    update about and add contact information

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,26 +1,59 @@
 import "./About.css";
 import AboutImg from "../../assets/about-2.svg";
+import { contacts, socialLinks } from "../../data";
 
 const About = () => {
   return (
-    <section className="sections">
+    <section className="sections" id="about">
       <h1 className="title">About Me</h1>
       <div className="title-underline"></div>
-      <div className="about-container">
+      <div className="about-contact-container">
         <div className="about-img-container">
           <img src={AboutImg} alt="about-img" className="about-img" />
         </div>
-        <div className="about-info">
-          <p>
-            Hello! I am currently an undergraduate student University of Colombo
-            School of Computing. I am passionate about software development, and I am constantly working on improving my
-            skills. I am a self-starter and take a proactive
-            approach to learning, often teaching myself new programming
-            languages and technologies in my free time. I am committed to always
-            giving my best effort. I am excited to continue learning and growing
-            as a programmer, and I am looking forward to seeing where my career
-            in the field will take me.
-          </p>
+        <div className="about-right-column">
+          <div className="about-container">
+            <div className="about-info">
+              <p>
+                Hello! I am currently a BIT undergraduate student University of
+                Colombo School of Computing. I am passionate about software
+                development, and I am constantly working on improving my skills.
+                I am a self-starter and take a proactive approach to learning,
+                often teaching myself new programming languages and technologies
+                in my free time. I am committed to always giving my best effort.
+                I am excited to continue learning and growing as a programmer,
+                and I am looking forward to seeing where my career in the field
+                will take me.
+              </p>
+            </div>
+          </div>
+
+          <div className="contact-container">
+            <h2>Contact Me</h2>
+            <div>
+              {contacts.map((contact) => {
+                const { id, details, url, icon } = contact;
+                return (
+                  <a href={url} key={id} className="contact-details">
+                    {icon}
+                    {details}
+                  </a>
+                );
+              })}
+            </div>
+            <div className="about-socials">
+              {socialLinks.map((link) => {
+                const { id, url, icon } = link;
+                return (
+                  <div className="hero-social-icon" key={id}>
+                    <a href={url} target="_blank">
+                      {icon}
+                    </a>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </section>
